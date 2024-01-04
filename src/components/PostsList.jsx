f66f04c1364c983e23759925a1df7d265ef228dc
Djennkins <djennkins228@gmail.com>
2024-01-04 23:17:32 +0200

    preparing the app for state management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,18 @@
 import Post from './Post'
+import NewPost from './NewPost'
 import styles from './PostsList.module.css'
 
 function PostsList() {
-	return <ul className={styles.posts}>
-		<Post author="Maximilian" body = "React is awesome!"/>
-		<Post author= "Manuel" body="Check out the full course!"/>
-	</ul>
+	return (
+		<>
+		<NewPost/>
+		<ul className={styles.posts}>
+			<Post author="Maximilian" body = "React is awesome!"/>
+			<Post author= "Manuel" body="Check out the full course!"/>
+		</ul>
+		</>
+	)
+	
 }
 
 export default PostsList;
