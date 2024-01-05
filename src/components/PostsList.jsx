1c2189333829b91c557f2459a15a5ea01d2e5dc6
Djennkins <djennkins228@gmail.com>
2024-01-05 18:26:23 +0200

    State & Conditional Content

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,9 +5,13 @@ import styles from './PostsList.module.css'
 import { useState } from 'react'
 
 function PostsList() {
-
+	const [modalIsVisible, setModelIsVisible] = useState(true);
 	const [enteredBody, setEnteredBody] = useState('');
 	const [enteredAuthor, setEnteredAuthor] = useState('');
+
+	function hideModalHandler() {
+		setModelIsVisible(false);
+	}
 
 	function bodyChangeHandler(event) {
 		setEnteredBody(event.target.value);
@@ -19,12 +23,12 @@ function PostsList() {
 
 	return (
 		<>
-			<Modal>
+			{ modalIsVisible && <Modal onClose={hideModalHandler}>
 				<NewPost
 					onBodyChange={bodyChangeHandler}
 					onAuthorChange={authorChangeHandler}
 				/>	
-			</Modal>
+			</Modal>}
 			<ul className={styles.posts}>
 				<Post author={enteredAuthor} body={ enteredBody} />
 				<Post author= "Manuel" body="Check out the full course!"/>
